e24f9f7a869ac01db9160ed57ef7fc113e3b3b70
Jakub <jakubkmn12@gmail.com>
2024-11-04 14:12:23 +0100

    Enabled multiple image uploading

--- frontend/main.py
+++ frontend/main.py
@@ -5,21 +5,22 @@ import io
 
 st.title("Dog or Cat Classifier")
 
-uploaded_file = st.file_uploader("Upload an image of a dog or cat", type=['jpg', 'jpeg', 'png'])
+uploaded_file = st.file_uploader("Upload an image of a dog or cat", type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image")
-    
-    img_byte_arr = io.BytesIO()
-    image.save(img_byte_arr, format='JPEG')  # Save as JPEG to match backend requirements
-    img_byte_arr.seek(0)  # Reset pointer to the beginning of the stream
+    for file in range(len(uploaded_file)):
+        image = Image.open(uploaded_file[file])
+        st.image(image, caption="Uploaded Image")
+        
+        img_byte_arr = io.BytesIO()
+        image.save(img_byte_arr, format='JPEG')  # Save as JPEG to match backend requirements
+        img_byte_arr.seek(0)  # Reset pointer to the beginning of the stream
 
-    files = {'file': ('uploaded_image.jpg', img_byte_arr, 'image/jpeg')}
-    response = requests.post("http://127.0.0.1:8000/predict/", files=files)
+        files = {'file': ('uploaded_image.jpg', img_byte_arr, 'image/jpeg')}
+        response = requests.post("http://127.0.0.1:8000/predict/", files=files)
 
-    if response.status_code == 200:
-        prediction = response.json()
-        st.write(f"Prediction: {prediction['prediction']}")
-    else:
-        st.write("Error in prediction: " + response.text)
+        if response.status_code == 200:
+            prediction = response.json()
+            st.write(f"Prediction: {prediction['prediction']}")
+        else:
+            st.write("Error in prediction: " + response.text)
